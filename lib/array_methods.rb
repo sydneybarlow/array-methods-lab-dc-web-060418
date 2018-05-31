2d7df46ea1e7afeded624db80c8a0a79fe090f45
Sydney Barlow <sydneybarlow44@gmail.com>
2018-05-31 02:33:45 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,22 +2,22 @@ def using_include(array, element)
   array.include?(element)
 end
 
-def using_sort(array)
+def using_sort(array, element)
   array.sort(element)
 end
 
-def using_reverse(array)
+def using_reverse(array, element)
   array.reverse(element)
 end
 
-def using_first(array)
+def using_first(array, element)
   array.first(element)
 end
 
-def using_last(array)
+def using_last(array, element)
   array.last(element)
 end
 
-def using_size(array)
+def using_size(array, element)
   array.size(element)
 end
